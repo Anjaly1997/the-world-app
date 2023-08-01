60b8c41c79d4dea91e5d8ffeef11c774fd3734fb
Anjaly1997 <anjaly1997soman@gmail.com>
2023-08-01 18:52:17 +0200

    Implemented Card Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-
+import CountryCard from "./Components/CountryCards";
+import './App.css'
 function App() {
   return (
     <div className="App">
-     hello
+     <div className="country-card-wrapper">
+     <CountryCard 
+      name = {'India'}
+      capital = {'Delhi'}
+      population = {120000000}
+      flagUrl = {'https://media.istockphoto.com/id/460844989/photo/the-indian-flag-with-horizontal-tricolor-design.jpg?s=612x612&w=0&k=20&c=HQqZ7MoU2RvkoD0VYdmR8Y-XWtuNifdaPPso8ow15CU='}
+     />
+     <CountryCard 
+      name = {'Germany'}
+      capital = {'Berlin'}
+      population = {120000000}
+      flagUrl = {'https://img.freepik.com/premium-vector/germany-flag-flag-germany-vector-illustration_685751-83.jpg'}
+     />
+     <CountryCard 
+      name = {'Germany'}
+      capital = {'Berlin'}
+      population = {120000000}
+      flagUrl = {'https://img.freepik.com/premium-vector/germany-flag-flag-germany-vector-illustration_685751-83.jpg'}
+     />
+     <CountryCard 
+      name = {'Germany'}
+      capital = {'Berlin'}
+      population = {120000000}
+      flagUrl = {'https://img.freepik.com/premium-vector/germany-flag-flag-germany-vector-illustration_685751-83.jpg'}
+     />
+     <CountryCard 
+      name = {'Germany'}
+      capital = {'Berlin'}
+      population = {120000000}
+      flagUrl = {'https://img.freepik.com/premium-vector/germany-flag-flag-germany-vector-illustration_685751-83.jpg'}
+     />
+     <CountryCard 
+      name = {'Germany'}
+      capital = {'Berlin'}
+      population = {120000000}
+      flagUrl = {'https://img.freepik.com/premium-vector/germany-flag-flag-germany-vector-illustration_685751-83.jpg'}
+     />
+     </div>
     </div>
   );
 }
